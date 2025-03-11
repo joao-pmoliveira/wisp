ac723377d3539c38429f300e5e49e8dfe3877b29
João Oliveira <joaop.oliveira@ua.pt>
2025-03-11 15:50:32 +0000

    Setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { createServer } from "node:http";
+import express from "express";
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello send!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
